cf319b4b030933b7a27ea17b67748bccba3389b2
Garbi93 <iseung2093@gmail.com>
2024-06-10 14:06:56 +0900

    6/10 스픨ㅇ OAuth2 클라이언테 세션 2 : 프로젝트 생성 및 의존성 추가 (main page 와 my page 추가)

---
+++ src/main/java/com/example/testsecurityoauthsession20240610/TestSecurityOAuthSession20240610Application.java
@@ -0,0 +1,13 @@
+package com.example.testsecurityoauthsession20240610;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecurityOAuthSession20240610Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSecurityOAuthSession20240610Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testsecurityoauthsession20240610/TestSecurityOAuthSession20240610ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsecurityoauthsession20240610;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecurityOAuthSession20240610ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
